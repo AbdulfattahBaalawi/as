58982ded1db776c50c272f1fe0d8910fcdb772ee
Abdulfattah E. Baalawi <55627035+AbdulfattahBaalawi@users.noreply.github.com>
2024-03-03 18:17:34 +0000

    Final version

--- utils.py
+++ utils.py
@@ -1,28 +1,19 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.impute import SimpleImputer 
+import re
 
-import inspect
-import textwrap
+class PrepProcesor(BaseEstimator, TransformerMixin): 
+    def fit(self, X, y=None): 
+        self.ageImputer = SimpleImputer()
+        self.ageImputer.fit(X[['Age']])        
+        return self 
+        
+    def transform(self, X, y=None):
+        X['Age'] = self.ageImputer.transform(X[['Age']])
+        X['CabinClass'] = X['Cabin'].fillna('M').apply(lambda x: str(x).replace(" ", "")).apply(lambda x: re.sub(r'[^a-zA-Z]', '', x))
+        X['CabinNumber'] = X['Cabin'].fillna('M').apply(lambda x: str(x).replace(" ", "")).apply(lambda x: re.sub(r'[^0-9]', '', x)).replace('', 0) 
+        X['Embarked'] = X['Embarked'].fillna('M')
+        X = X.drop(['PassengerId', 'Name', 'Ticket','Cabin'], axis=1)
+        return X
 
-import streamlit as st
-
-
-def show_code(demo):
-    """Showing the code of the demo."""
-    show_code = st.sidebar.checkbox("Show code", True)
-    if show_code:
-        # Showing the code of the demo.
-        st.markdown("## Code")
-        sourcelines, _ = inspect.getsourcelines(demo)
-        st.code(textwrap.dedent("".join(sourcelines[1:])))
+columns = ['PassengerId', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch','Ticket', 'Fare', 'Cabin', 'Embarked']
